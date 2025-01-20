18e7b7c05f3ddd866aed6a58f3c2e3250bbd2042
IlnarTwo <ilasurygin4@gmail.com>
2025-01-20 19:14:38 +0700

    update 20.01.25 19:14

--- php/auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: http://localhost:3000');
-//$email = $_POST["email"];
-//$passw = $_POST["passw"];
-//echo "Ваш email: ".$email."<br> Ваш пароль: ".$passw;
-var_dump($_POST)
-?>
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,62 +1,77 @@
-import React from "react"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
-import { Button, Col, Form, Row } from "react-bootstrap"
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { Button, Col, Form, Row } from "react-bootstrap";
 
-class Auth extends React.Component{
-  constructor(props){
-    super(props)
+class Auth extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
       email: "",
       passw: ""
-    }
+    };
 
-    this.onForm = this.onForm.bind(this)
-    this.handleChangeEmail = this.handleChangeEmail.bind(this)
-    this.handleChangePassw = this.handleChangePassw.bind(this)
+    this.onForm = this.onForm.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassw = this.handleChangePassw.bind(this);
   }
 
   async onForm(event) {
-    event.preventDefault()
-    let info = {
+    event.preventDefault();
+
+    const info = {
       email: this.state.email,
       passw: this.state.passw
-    }
-    let response = await fetch("http://127.0.0.1/server/php/auth.php", {
-      mode: "no-cors",
-      method:"POST",
-      body: JSON.stringify(info),
-      headers: {
-       'Content-Type': 'application/json;charset=utf-8'
+    };
+
+    try {
+      const response = await fetch("http://127.0.0.1/server/php/auth.php", {
+        method: "POST",
+        body: JSON.stringify(info),
+        headers: {
+          "Content-Type": "application/json;charset=utf-8"
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.status}`);
       }
-    })
-    .then(response => response.json())
-    .then(commits => console.log(commits[0]))
 
-    console.log(info)
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("There has been a problem with your fetch operation:", error);
+    }
 
-    console.log("1234")
+    console.log(info);
+    console.log("1234");
   }
 
   handleChangeEmail(event) {
-    this.setState({email: event.target.value})
-  }
-  handleChangePassw(event){
-    this.setState({passw: event.target.value})
+    this.setState({ email: event.target.value });
   }
 
-  render(){
-    return(
+  handleChangePassw(event) {
+    this.setState({ passw: event.target.value });
+  }
+
+  render() {
+    return (
       <div>
         <Header title="Авторизация" />
         <div className="container">
-          <Form onSubmit={this.onForm}  method="post" className="col-md-8 mx-auto">
+          <Form onSubmit={this.onForm} method="post" className="col-md-8 mx-auto">
             <Form.Group as={Row} className="justify-content-md-center mb-3" controlId="formAuthEmail">
               <Form.Label column sm="2">
                 Email
               </Form.Label>
               <Col sm="5">
-              <Form.Control type="email" placeholder="exampl@email.com" name="email" onChange={this.handleChangeEmail}/>
+                <Form.Control
+                  type="email"
+                  placeholder="example@email.com"
+                  name="email"
+                  onChange={this.handleChangeEmail}
+                />
               </Col>
             </Form.Group>
             <Form.Group as={Row} className="justify-content-md-center mb-3" controlId="formAuthPassw">
@@ -64,16 +79,22 @@ class Auth extends React.Component{
                 Password
               </Form.Label>
               <Col sm="5">
-              <Form.Control type="password" name="passw" onChange={this.handleChangePassw}/>
+                <Form.Control 
+                  type="password" 
+                  name="passw" 
+                  onChange={this.handleChangePassw} 
+                />
               </Col>
             </Form.Group>
-            <Button type="submit" className="col-md-5 mx-auto">Sign in</Button>
+            <Button type="submit" className="col-md-5 mx-auto">
+              Sign in
+            </Button>
           </Form>
         </div>
         <Footer />
       </div>
-    )
+    );
   }
 }
 
-export default Auth
+export default Auth;
