7ebfbaccf93efbe92d4ad907f273b307f1e59bb6
IRuGaSI <92851414+IlnarTwo@users.noreply.github.com>
2025-01-24 14:10:29 +0700

    update da

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Pages from './components/Pages';
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            page: "admin",
+            authentificate: false
+        }
+
+    } 
+
+    async getData() {
+        const url = "http://127.0.0.1/server/php/authentificate.php";
+        try {
+            const response = await fetch(url, {mode: "no-cors"});
+            console.log(response.status);
+            const json = await response.json();
+            console.log(json);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                <Pages page={this.state.page}/>
+            </div>
+        )
+    }
+
+    
+      
+
+}
+
+export default App
