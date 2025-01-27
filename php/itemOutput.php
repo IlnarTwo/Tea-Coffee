f4f9e125734ddb924d7e781864e8998a4c1c0eda
IlnarTwo <ilasurygin4@gmail.com>
2025-01-27 17:34:57 +0700

    update 27.01.25 17:34

---
+++ php/itemOutput.php
@@ -0,0 +1,12 @@
+<?php
+header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+
+$json = file_get_contents('php://input');
+$obj = json_decode($json, true);
+
+require_once("db.php");
+
+?>
