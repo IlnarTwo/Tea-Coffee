fdce819089f7ddea0772d2903c9a0044123f01de
IlnarTwo <ilasurygin4@gmail.com>
2025-01-31 21:13:43 +0700

    update 31.01.25 21:13

--- php/addItem.php
+++ php/addItem.php
@@ -3,6 +3,7 @@ header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+header("Access-Control-Allow-Credentials: true");
 
 $json = file_get_contents('php://input');
 $obj = json_decode($json, true);
--- php/users.php
+++ php/users.php
@@ -1,3 +1,24 @@
 <?php
+header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+header("Access-Control-Allow-Credentials: true");
+session_start();
 
+$json = file_get_contents('php://input');
+$obj = json_decode($json, true);
+
+require_once("db.php");
+
+$authSql ="SELECT * FROM `user`";
+$res = $conn->query($authSql);
+
+$users = $res->fetch(PDO::FETCH_ASSOC);
+
+var_dump($users);
+
+var_dump($res);
+
+echo json_encode($users);
 ?>
