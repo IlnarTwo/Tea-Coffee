b9923f094e47601179f414db9a7e41939be7d448
IlnarTwo <ilasurygin4@gmail.com>
2025-02-04 15:12:29 +0700

    update 04.02.25 15:12

--- php/orders.php
+++ php/orders.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Password');
-header("Access-Control-Allow-Credentials: true");
+// header("Access-Control-Allow-Credentials: true");
 session_start();
 
 $json = file_get_contents('php://input');
