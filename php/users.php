9cac77ea126ea7910de547b67b9694d20a5a5a7d
IlnarTwo <ilasurygin4@gmail.com>
2025-03-16 16:09:10 +0700

    up

--- php/users.php
+++ php/users.php
@@ -3,18 +3,15 @@ header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Password');
-// header("Access-Control-Allow-Credentials: true");
 session_start();
-
-$json = file_get_contents('php://input');
-$obj = json_decode($json, true);
 
 require_once("db.php");
 
-$authSql ="SELECT * FROM `user`";
+$authSql = "SELECT * FROM `user`";
 $res = $conn->query($authSql);
 
-$users = $res->fetch(PDO::FETCH_ASSOC);
+// Используем fetchAll для получения всех строк
+$users = $res->fetchAll(PDO::FETCH_ASSOC);
 
-echo json_encode($users);
+echo json_encode(["users" => $users]); // Возвращаем данные в формате { "users": [...] }
 ?>
